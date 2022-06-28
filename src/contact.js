6343382f70861e8d57d9475be9d741d5f78bf9b9
EHoldener <eholdener12@gmail.com>
2022-06-28 16:39:43 -0700

    wrote content for contact page

--- src/contact.js
+++ src/contact.js
@@ -1,3 +1,34 @@
 export default function contactPage() {
-  console.log("another test");
+  let contactContainer = document.createElement("div");
+
+  let row1 = document.createElement("div");
+
+  let contactHead = document.createElement("h1");
+  contactHead.innerHTML = "Contact Us";
+
+  let contactCard = document.createElement("div");
+
+  let contactName = document.createElement("h2");
+  contactName.innerHTML = "The Mad Hatter";
+
+  let contactTitle = document.createElement("p");
+  contactTitle.innerHTML = "Owner and Proprieter";
+
+  let contactPhone = document.createElement("p");
+  contactPhone.innerHTML = "555-555-5555";
+  
+  let contactEmail = document.createElement("p");
+  contactEmail.innerHTML = "definetlyreal@notfake.com";
+
+  contactCard.appendChild(contactName);
+  contactCard.appendChild(contactTitle);
+  contactCard.appendChild(contactPhone);
+  contactCard.appendChild(contactEmail);
+
+  row1.appendChild(contactHead);
+  row1.appendChild(contactCard);
+
+  contactContainer.appendChild(row1);
+
+  return contactContainer
 }
